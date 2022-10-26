014438bb5f802e5e2248c881d665ad6a12da4f03
jacobrdavis <jacobdavis4085@gmail.com>
2022-10-25 21:59:48 -0400

    telemetry report function

--- telemetry/compile_SBD.py
+++ telemetry/compile_SBD.py
@@ -10,8 +10,10 @@ Contents:
 Log:
     - 2022-09-06, J.Davis: created
     - 2022-09-12, J.Davis: updated doc strs, convert dataframe index to datetime index
+    - 2022-09-16, J.Davis: added dict sorting with sort_dict()
 
 """
+import numpy as np
 from pandas import DataFrame, to_datetime
 from read_SBD import read_SBD
 from typing import Any
@@ -19,7 +21,7 @@ from typing import Any
 def to_pandas_datetime_index(
     df: DataFrame,
     datetimeColumnName: str = 'datetime',
-) -> DataFrame:
+)-> DataFrame:
     """
     Convert a pandas.DataFrame integer index to a pandas.DatetimeIndex (in place)
 
@@ -37,24 +39,45 @@ def to_pandas_datetime_index(
     # df.drop(['datetime'], axis=1, inplace=True)
     return
 
+def sort_dict(
+    d: dict,
+)-> dict:
+    """
+    Sort each key of a dictionary containing microSWIFT data based on the 
+    key containing datetime information.
+
+    Arguments:
+        - d (dict), unsorted dictionary
+            * Must contain a 'datetime' key with a list of datetime objects
+
+    Returns:
+        - (dict), sorted dictionary
+    """
+    sortIdx = np.argsort(d['datetime'])
+    dSorted = {}
+    for key, val in d.items():
+        dSorted[key] = np.array(val)[sortIdx]
+
+    return dSorted
+
 
 def compile_SBD(
     SBDfolder: str, 
     varType: str, 
     fromMemory: bool = False
-) -> Any:
+)-> Any:
     """
     Compile contents of short burst data files into the specified variable 
     type or output.
 
     Arguments:
-        - SBDfolder (str), _description_
-        - varType (str), _description_
-        - fromMemory (bool, optional), _description_; defaults to False.
+        - SBDfolder (str), directory (virtual or local) containing .sbd files
+        - varType (str), variable type to be returned
+        - fromMemory (bool, optional), flag to indicate whether SBDfolder was 
+              loaded from memory (True) or a local file (False); defaults to False.
 
     Raises:
-        - Exception, _description_
-        - ValueError, _description_
+        - ValueError, varType can only be 'dict', 'pandas', or 'xarray'
 
     Returns:
         - (dict), if varType == 'dict'
@@ -62,24 +85,25 @@ def compile_SBD(
         See pull_telemetry_as_var() for definitions
 
     """
-    SWIFT = []
+    data = []
 
     if fromMemory == True:
         for file in SBDfolder.namelist():
-            SWIFT.append(read_SBD(SBDfolder.open(file)))
+            data.append(read_SBD(SBDfolder.open(file)))
 
     else: #TODO: support reading from a folder of SBDs?
         for SBDfile in SBDfolder: 
             with open(SBDfile, mode='rb') as file: # b is important -> binary
                 # fileContent = file.read()
-                SWIFT.append(read_SBD(file))
+                data.append(read_SBD(file))
 
     if varType == 'dict':
-        return {k: [d.get(k) for d in SWIFT] for k in set().union(*SWIFT)}
+        d = {k: [d.get(k) for d in data] for k in set().union(*data)}
+        return sort_dict(d)
     
     elif varType == 'pandas':
         import pandas
-        df = pandas.DataFrame(SWIFT)
+        df = pandas.DataFrame(data)
         if not df.empty:
             to_pandas_datetime_index(df)
         return df
---
+++ telemetry/create_telemetry_report.py
@@ -0,0 +1,225 @@
+"""
+Author: @jacobrdavis
+
+A collection of python functions for accessing data from the UW-APL SWIFT server:
+http://swiftserver.apl.washington.edu/ (base URL)
+http://faculty.washington.edu/jmt3rd/SWIFTdata/DynamicDataLinks.html (HTML page)
+
+See pull_telemetry_example.ipynb for a complete tutorial.
+
+
+Contents:
+    - create_request()
+    - pull_telemetry_as_var()
+    - pull_telemetry_as_zip()
+    - pull_telemetry_as_json()
+    - pull_telemetry_as_kml()
+
+Log:
+    - 2022-09-06, J.Davis: created
+    - 2022-09-12, J.Davis: updated doc strs, added datetime indexing to dataframes, requirements.txt
+
+"""
+from typing import Iterable
+from pull_telemetry import pull_telemetry_as_json, pull_telemetry_as_var
+from datetime import datetime, timezone, timedelta, tzinfo
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+#%%
+#TODO:
+# - pull config from branch?
+# -  
+def find_full_range(
+    X1: np.array,
+    X2: np.array,
+)-> tuple:
+    #TODO: doctstr
+    x_min = min([X1.min(), X2.min()])
+    x_max = max([X1.max(), X2.max()])
+    return (x_min, x_max)
+
+def date_range(
+    start: datetime,
+    end: datetime, 
+    msgPerHr: float,
+)-> pd.DatetimeIndex:
+    #TODO:
+    dateRange = pd.date_range(
+        start, 
+        end, 
+        freq = f'{msgPerHr}H', 
+        tz = timezone.utc
+    ) 
+    return dateRange
+
+def create_telemetry_report(
+    buoyID: str, 
+    startDate: datetime, 
+    endDate: datetime, 
+    burstInterval: int = None,
+)-> dict:
+
+
+
+    print('')
+
+
+def create_telemetry_report_figure(
+    serverTimestamps, 
+    onboardTimestamps,
+    allCallWindows,
+):
+
+    fig,ax = plt.subplots(figsize = (8, 3))
+
+    ax.hist(
+        serverTimestamps,
+        bins= allCallWindows,
+        color='red',
+        edgecolor='red',
+        alpha=0.5,
+        label='received'
+    )
+
+    ax.hist(
+        onboardTimestamps,
+        bins= allCallWindows,
+        color='royalblue',
+        edgecolor='royalblue',
+        alpha=0.5,
+        label='recorded'
+    )
+
+    ax.axvline(
+        start,
+        color='darkorange',
+        alpha=0.75,
+        linewidth = 1.5,
+        label = 'query start',
+        clip_on = False,
+    )
+
+    ax.axvline(
+        end, 
+        color='darkorange',
+        alpha=0.75,
+        linewidth = 1.5,
+        label = 'query end',
+        clip_on = False,
+    )
+
+    dtFmt = mpl.dates.DateFormatter('%d%b %H:%M') # define the formatting
+    plt.gca().xaxis.set_major_formatter(dtFmt) 
+    plt.xticks(rotation = 90)
+
+    ylims = np.round(ax.get_ylim()) #+ np.array([0,1])
+    yticks = ax.get_yticks()
+    ax.set_yticks(np.unique(np.round(yticks,0)))
+    yminor_ticks = np.arange(0, ylims[1], 1)
+
+    ax.set_xticks(fullRange, minor=True)
+    ax.set_yticks(yminor_ticks, minor=True)
+
+    # grid settings:
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=0.2)
+
+    ax.set_xlim([minDate, maxDate])
+    ax.set_ylim(ylims)
+    ax.legend(loc='upper right')
+    ax.set_ylabel('messages per burst window')
+
+#%%
+start = datetime(2022,9,26,0,0,0)
+end   = datetime(2022,9,30,0,0,0)
+buoyID = '019'
+buoyID = '057'
+
+
+# start = datetime(2022,10,15,12,0,0)
+# buoyID = '005'
+# buoyID = '068'
+burstInterval = None
+SWIFT_json = pull_telemetry_as_json(buoyID = buoyID, startDate = start)
+
+
+serverTimes = [msg['timestamp'] for msg in SWIFT_json['buoys'][0]['data']]
+
+serverTimes = pd.to_datetime(serverTimes, format='%Y-%m-%dT%H:%M:%S%Z') # datetime.strptime(serverTimes[0], '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+
+SWIFT_dict = pull_telemetry_as_var(buoyID = buoyID, startDate = start, endDate = end, varType = 'dict')
+
+if burstInterval is None:
+    msgRate = np.diff(SWIFT_dict['datetime'])
+    msgPerHr = np.round(np.mean(msgRate[msgRate > timedelta(0)]).total_seconds() / 3600)
+    print(f'Inferred burst rate of {msgPerHr} messages per hour.')
+else:
+    msgPerHr = burstInterval/60
+
+# end = datetime.utcnow().replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)#TODO: make fun arg default
+
+startRange = SWIFT_dict['datetime'].min().replace(minute=0, second=0)
+endRange = SWIFT_dict['datetime'].max().replace(minute=0, second=0)
+
+dateRange = date_range(startRange, endRange, msgPerHr)
+dateRangeQuery = date_range(start, end, msgPerHr)
+minDate, maxDate = find_full_range(dateRange, dateRangeQuery)
+fullRange = date_range(minDate, maxDate, msgPerHr)
+
+# inTimes = np.searchsorted(dateRange.tolist(), SWIFT_dict['datetime'], side='left')
+
+# [dateRange[i] for i in inTimes]
+# inTimesServer = np.searchsorted(dateRange.tolist(), serverTimes, side='left')
+
+#%%
+create_telemetry_report_figure(serverTimes, SWIFT_dict['datetime'], dateRange)
+
+
+
+#%%
+fig,ax = plt.subplots()
+sns.histplot([dateRange[i] for i in inTimes],
+bins= len(dateRange),
+ ax=ax)
+plt.xticks(rotation = 90)
+
+# hist, bin_edges = np.histogram(inTimesServer, bins=len(dateRange)+1)
+
+
+fig,ax = plt.subplots()
+ax.hist([dateRange[i] for i in inTimesServer],
+    bins= dateRange,
+    alpha=0.5,
+)
+
+ax.hist([dateRange[i] for i in inTimes],
+bins= dateRange,
+alpha=0.5,
+)
+plt.xticks(rotation = 90)
+
+
+fig,ax = plt.subplots()
+sns.histplot(SWIFT_dict['datetime'],
+# bins= len(SWIFT_dict['datetime']),
+ ax=ax)
+# scatter(dn2dt([microSWIFT017.time]),1*ones(size([microSWIFT017.time])),'filled','color',[0.0000 0.4470 0.7410])
+
+
+fig,ax = plt.subplots()
+ax.scatter(SWIFT_dict['datetime'], np.ones(np.shape(SWIFT_dict['datetime'])))
+ax.scatter(dateRange, np.ones(np.shape(dateRange)), color='none', edgecolor='k')
+ax.axvline(start)
+ax.axvline(end)
+plt.xticks(rotation = 90)
+
+
+#%%
+
+
+#%%
+
+
